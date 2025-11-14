26455aca15dcf6c3cc94294663dc0819c8904b7d
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-11-14 06:58:49 -0500

    get mapping for student worked

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,9 +1,13 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.annotation.Primary;
 
 @Entity
+@Getter
+@Setter
 public class Student {
 
     @Id
