9988f4fffcc99383b12eebf429079879441f2ae3
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-11-20 07:44:16 -0500

    add env file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import me.paulschwarz.springdotenv.DotenvConfig;
+import me.paulschwarz.springdotenv.DotenvPropertyLoader;
+import me.paulschwarz.springdotenv.DotenvPropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    @Autowired
+    Environment env;
+
+    @PostConstruct
+    public void testEnv(){
+        System.out.println("DB url := " + env.getProperty("DB_URL"));
+    }
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+        Dotenv dotenv = Dotenv.configure().directory("./").ignoreIfMissing().load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
